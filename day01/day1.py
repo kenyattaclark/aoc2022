a8a7c35d3c1e778cce3faedb5a8e0d9d624cfabd
Kenyatta Clark <kenyatta.clark@gmail.com>
2022-12-02 09:52:46 -0600

    refactor to make more stream line

--- day01/day1.py
+++ day01/day1.py
@@ -3,24 +3,15 @@ content = f.read()
 l = content.split('\n')
 
 currentCalories = 0
-mostCalories = 0
-currentElf = 0
-elfWithMostCalories = 0
 calorieArray = []
 
 for s in l:
     if s != '':
-        # print(f'Working on elf number: {currentElf + 1} -> {currentCalories} + {s}')
         currentCalories = currentCalories + int(s)
     else:
-        currentElf = currentElf + 1
-        # print(f'Elf number {currentElf} is carrying {currentCalories} calories')
         calorieArray.append(currentCalories)
-        if currentCalories > mostCalories:
-            mostCalories = currentCalories
-            elfWithMostCalories = currentElf
         currentCalories = 0
 
-print(f'Part 1: The elf carrying the most calories is number {elfWithMostCalories} and they are carrying {mostCalories}.')
 calorieArray.sort(reverse=True)
+print(f'Part 1: The most calories carried by an elf is {calorieArray[0]}')
 print(f'Part 2: Top 3 Total: {calorieArray[0] + calorieArray[1] + calorieArray[2]}')
